802f575cbe754552cdcd742f22a367b0d9251a40
K4444R <kingzalishev@gmail.com>
2025-03-03 22:11:45 +0500

    test commit

--- front-end/src/components/GradesPage.vue
+++ front-end/src/components/GradesPage.vue
@@ -94,7 +94,7 @@ const fetchGrades = async () => {
       return;
     }
 
-    const response = await axios.get('https://b8f2-37-99-49-122.ngrok-free.app/api/auth-api/grades/getGrades?year=2024&semester=1', {
+    const response = await axios.get('https://2bbc-37-99-48-152.ngrok-free.app/api/auth-api/grades/getGrades?year=2024&semester=1', {
       headers: {
         Authorization: `Bearer ${token}`,
       },
