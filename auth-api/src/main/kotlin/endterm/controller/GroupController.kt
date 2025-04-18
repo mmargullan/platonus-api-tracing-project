7b6adc6bf58571ef2f4c500e0f010dbc9a99124d
Margulan <m.begzhan@aster.kz>
2025-04-18 14:53:34 +0500

    bug fix

--- auth-api/src/main/kotlin/endterm/controller/GroupController.kt
+++ auth-api/src/main/kotlin/endterm/controller/GroupController.kt
@@ -29,9 +29,9 @@ class GroupController(
         return groupService.getAllStudents(groupId)
     }
 
-    @GetMapping("/getStudentRating/{groupId}")
-    fun getStudentRating(@PathVariable groupId: Long): Int {
-        return groupService.getStudentRating(groupId)
-    }
+//    @GetMapping("/getStudentRating/{groupId}")
+//    fun getStudentRating(@PathVariable groupId: Long): Int {
+//        return groupService.getStudentRating(groupId)
+//    }
 
 }
